3490aec3ab3cb65e8f6bfd9f98ac3b0ea21843de
Nicolas Gomez <nico.2000.03.nmg10@gmail.com>
2025-08-05 09:50:32 -0300

    Programacion4: quedandome con el cursor.jsx correcto

--- Programacion4/front/cf/src/cursor.jsx
+++ Programacion4/front/cf/src/cursor.jsx
@@ -5,30 +5,27 @@ export default function Cursor() {
   const [border, setBorder] = useState("border-green-600");
   useEffect(() => {
     setTimeout(() => {
-      setBorder("border-blue-600"), 1000;
-    });
+      setBorder("border-blue-600");
+    }, 1000);
     return () => setBorder("border-red-600");
   }, []);
   //tarea. Hacer que el icono siga al cursor. Usar la API windows.client, hacer con style no className
   const [pos, setPos] = useState({ x: 0, y: 0 });
+
   useEffect(() => {
-    const handleMouse = (e) => {
-      setPos({
-        x: e.ClientX,
-        y: e.ClientY,
-      });
+    const handleMouseMove = (e) => {
+      setPos({ x: e.clientX - 40, y: e.clientY - 40 });
     };
-    console.log(pos);
-    document.addEventListener("mousemove", handleMouse);
+    document.addEventListener("mousemove", handleMouseMove);
     return () => {
-      document.removeEventListener("mousemove", handleMouse);
+      document.removeEventListener("mousemove", handleMouseMove);
     };
   }, []);
   return (
     <div
-      style={{ transform: `translate(${pos.x}px, ${pos.y}px)` }}
+      style={{ transform: `translate(${pos.x}px  , ${pos.y}px)` }}
       className={`${border} z-[-1] top-0 left-0 bg-gray-600 
-        absolute w-20 h-20  border-2 rounded-full transition duration-200
+        absolute w-20 h-20 border-2 rounded-full transition duration-200
         `}
     ></div>
   );
