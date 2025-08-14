d136d978e5d625ee22154e9a83478e897a0d80c9
Nicolas Gomez <nico.2000.03.nmg10@gmail.com>
2025-08-14 09:48:53 -0300

    Programacion 4: tug of war - se refactorizo funciones

--- Programacion4/front/tug-of-war/components/game.jsx
+++ Programacion4/front/tug-of-war/components/game.jsx
@@ -2,38 +2,41 @@ import { useState } from "react";
 import Team from "./team";
 
 export default function Game() {
-    const [cuerdaEquipos, setCuerdaEquipos] = useState({
-        red:5, green: 5,
-    })
-    //simplificar este codigo aun más
-    const handleTirar = (color)=>{
-        
-        if(color === "red"){
-            const green = cuerdaEquipos.green - 1;
-            setCuerdaEquipos({
-                green,
-                [color]: cuerdaEquipos[color] +1,
-            })
-        }else{
-            const red = cuerdaEquipos.red - 1;
-            setCuerdaEquipos({
-                red,
-                [color]: cuerdaEquipos[color] +1,
-            })
-        }
-    }
+  const [cuerdaEquipos, setCuerdaEquipos] = useState({
+    red: 5,
+    green: 5,
+  });
 
-    return (
-        <>
-        <h1 style={{margin:"0 auto"}}>Tug of war</h1>
-    <main style={{display:"flex", alignItems:"center", gap:"20px"}}>
-        <Team color="green" cantCuerda={cuerdaEquipos.green} onTirar={handleTirar}/>
+  //simplificar este codigo aun máa
+  const handleTirar = (color) => {
+    const oppositeColor = Object.keys(cuerdaEquipos).filter(
+      (c) => c !== color
+    )[0];
+    setCuerdaEquipos((prev) => ({
+      [color]: prev[color] + 1,
+      [oppositeColor]: prev[oppositeColor] - 1,
+    }));
+  };
+
+  return (
+    <>
+      <h1 style={{ margin: "0 auto" }}>Tug of war</h1>
+      <main style={{ display: "flex", alignItems: "center", gap: "20px" }}>
+        <Team
+          color="green"
+          cantCuerda={cuerdaEquipos.green}
+          onTirar={handleTirar}
+        />
         <div>
-        <div> Longitud: {cuerdaEquipos.green + cuerdaEquipos.red}</div>
-        ---------------------------------------------------
+          <div> Longitud: {cuerdaEquipos.green + cuerdaEquipos.red}</div>
+          ---------------------------------------------------
         </div>
-        <Team color="red" cantCuerda={cuerdaEquipos.red} onTirar={handleTirar}/>
-    </main>
+        <Team
+          color="red"
+          cantCuerda={cuerdaEquipos.red}
+          onTirar={handleTirar}
+        />
+      </main>
     </>
-  )
+  );
 }
--- Programacion4/front/tug-of-war/components/team.jsx
+++ Programacion4/front/tug-of-war/components/team.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
+import React from "react";
 
-export default function Team({color,cantCuerda,onTirar}) {
+export default function Team({ color, cantCuerda, onTirar }) {
   return (
     <div>
-    <h2> 
-        Equipo color: <span style={{color}}> {color} </span> 
-    </h2>
-    <p>Tirando: {cantCuerda}</p>
-    <button onClick={()=>onTirar(color)}>tirar 1 m</button>
+      <h2>
+        Equipo color: <span style={{ color }}> {color} </span>
+      </h2>
+      <p>Tirando: {cantCuerda}</p>
+      <button onClick={() => onTirar(color)}>tirar 1 m</button>
     </div>
-  )
+  );
 }
